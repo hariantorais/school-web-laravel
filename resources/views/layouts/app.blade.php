c65fb9e7a04bce772de7a86aef5867cb4bacbcff
Harianto Rais <ibnurais.abdillah@gmail.com>
2026-07-06 23:59:41 +0700

    favicon fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,10 +9,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
-     <link rel="icon" type="image/png" href="{{ asset('images/favicon.ico') }}">
-    <link rel="icon" type="image/x-icon" href="{{ asset('images/favicon.ico') }}">
-    <link rel="apple-touch-icon" href="{{ asset('images/favicon.ico') }}">
-    
+    <link rel="icon" type="image/png" href="{{ asset('images/favicon.png') }}">
+
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
